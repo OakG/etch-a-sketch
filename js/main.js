f61892388ce378734f1ada615263ee9d9c80048b
OakG <oak.garcia@gmail.com>
2016-09-26 01:23:42 -0300

    Experiments

--- js/main.js
+++ js/main.js
@@ -2,13 +2,8 @@
 $(document).ready(function() {
     $('#16x16').on('click', function() {
         for (var i = 0; i < 15; i++) {
-            if(i%2 != 0) {
                 $('.board').append('<div class="dot"></div>');
-            } else {
-                $('.board').append('<div class="ndot"></div>');
-            }
-
-        }
+        } // Only makes a line
 
     });
 
